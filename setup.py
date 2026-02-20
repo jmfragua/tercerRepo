509c5d0247bb856d775a6b201d5d06aacfdfef33
jose <mariofraguarengifo@gmail.com>
2026-02-20 18:48:17 -0500

    mi primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                              # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Mario fragua",                         # Tu nombre
+    author_email="mariofraguarengifo@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/jmfragua/tercerRepo",     # URL del proyecto
+)
